cbdb352f8021aeee27743bb1b2af3bb3ed70f5af
CloudedQuartz <ravenklawasd@gmail.com>
2021-04-29 13:33:32 +0530

    homeScreen: add homescreen with 2 buttons
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -4,7 +4,66 @@ import 'addnote.dart';
 
 void homeScreen() {
 
-  // directly run notes for now
-  runApp(StartNotes());
+  // run empty homescreen for now
+  runApp(HomeScreen());
 
+}
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+
+        // title
+        appBar: AppBar(title: Text('Home'), centerTitle: true),
+
+        // body
+        body: Padding(
+
+          padding: EdgeInsets.all(20),
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: <Widget>[
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [
+                  ElevatedButton(
+                    child: Text('Authentication'),
+                    onPressed: () {
+
+                    },
+                  ),
+
+
+                  SizedBox(height: 16.0),
+
+
+                  ElevatedButton(
+                    //pad edge
+                    style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(horizontal: 43)),
+                    child: Text('Notes'),
+                    onPressed: () {
+
+                    },
+                  )
+                ],
+
+              )
+            ]
+          ),
+
+
+
+        ),
+      ),
+    );
+
+  }
 }
